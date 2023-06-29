e8cd9a48f756ef239eaed76a3aab7eb8c8f2e4fa
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-06-29 19:31:39 +0200

    Expense structure

---
+++ public/js/expenses.js
@@ -0,0 +1,22 @@
+"use strict";
+class ArrayList {
+    constructor() {
+        this.items = [];
+    }
+    add(item) {
+        this.items.push(item);
+    }
+    get(index) {
+        const item = this.items.filter((x, i) => {
+            return i === index;
+        });
+        if (item.length === 0) {
+            return null;
+        }
+        else {
+            return item[0];
+        }
+    }
+}
+class Expenses {
+}
